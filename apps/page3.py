9cdc299f7118d456d5bd5e4c42576333a664b075
myriamBou <myriambouhlel@gmail.com>
2020-12-18 15:51:28 +0100

    update

--- apps/page3.py
+++ apps/page3.py
@@ -24,7 +24,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 
 # for visualization
-import seaborn as sns
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
